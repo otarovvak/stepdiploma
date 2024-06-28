d622e53a38d56df9f941de5b29034e5e7a2b1fac
otarovvak <134216287+otarovvak@users.noreply.github.com>
2024-06-28 20:16:21 +0500

    edit

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,6 @@ mongoose.connect(
 app.get("/", (req, res) => {
   res.send("Express App is running");
 });
-
 const orderSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
@@ -241,6 +240,22 @@ app.get("/popularproducts", async (req, res) => {
   console.log("popular products Fetched");
   res.send(popularproducts);
 });
+fetchUser = async (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ errors: "Please authenticate using valid login" });
+  } else {
+    try {
+      const data = jwt.verify(token, "secret_ecom");
+      req.user = data.user;
+      next();
+    } catch (error) {
+      res
+        .status(401)
+        .send({ errors: "please autheticate using a valid token" });
+    }
+  }
+};
 
 app.post("/addtocart", fetchUser, async (req, res) => {
   const { productId, selectedColor } = req.body;
